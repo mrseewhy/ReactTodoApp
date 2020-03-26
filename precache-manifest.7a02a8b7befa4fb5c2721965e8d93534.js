33404010cdf858bee03ab6224037e14c1478efab
Olusegun Adeyemi <segunisaboy@gmail.com>
2020-03-26 12:42:32 +0100

    first commit

---
+++ precache-manifest.7a02a8b7befa4fb5c2721965e8d93534.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "04a8a105af8c4a53a711cba3f55d939d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6aaf25fe86b7cabeb9e1",
+    "url": "/static/css/main.7f668dbb.chunk.css"
+  },
+  {
+    "revision": "fba734d02eb26cf51835",
+    "url": "/static/js/2.a7bcc0eb.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.a7bcc0eb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6aaf25fe86b7cabeb9e1",
+    "url": "/static/js/main.76edbf40.chunk.js"
+  },
+  {
+    "revision": "252f969779de0734efb3",
+    "url": "/static/js/runtime-main.75211130.js"
+  }
+]);
